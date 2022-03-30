790b1a33d2e34580d07addf95e2cf14de5dfb72c
abhay <imherobrine25@gmail.com>
2022-03-30 23:59:23 +0530

    unicode application

---
+++ namaste.java
@@ -0,0 +1,13 @@
+public class namaste {
+    // java uses unicode there for you can output any language
+    public static void main(String[] args) {
+        System.out.println("Hindi:नमस्ते");
+        System.out.println("Russian:Привет");
+        System.out.println("irish:Dia dhuit");
+        System.out.println("Filipino:Kamusta");
+        System.out.println("Urdu:ہیلو");
+        System.out.println("Thai:สวัสดี");
+        System.out.println("Chinese:你好");
+
+    }
+}

29ac4d221821821380793570b9e85f58b74759a8
abhay <imherobrine25@gmail.com>
2022-03-30 12:38:49 +0530

    hello world

---
+++ Helloworld.java
@@ -0,0 +1,11 @@
+public class Helloworld {
+    //All the functions in the classes are known as methods
+    //Static variables does not depend on the object
+    //main class don't need an object (most times) therefore it is static
+    public static void main(String[] args) {
+        //String[] args is array which stores arguments from commandline
+        //System is a class which contains many functions like println
+        // out is a variable of System class which is a Print Stream
+        System.out.println("Hello world");
+    }
+}

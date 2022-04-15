6604c8d08c5e487511aee7f16c23f5ae22b44d0c
abhay <imherobrine25@gmail.com>
2022-04-15 16:39:24 +0530

    Java Loops

---
+++ Loop.java
@@ -0,0 +1,30 @@
+//loops in java
+
+public class Loop {
+
+    public static void main(String[] args) {
+
+        //for loop
+
+        for (int num =1; num<=5; num++){
+            System.out.println(num);
+        }
+
+        //while loop
+
+        int num = 6 ;
+        while(num<=10){
+            System.out.println(num);
+            num++;
+        }
+
+        // do while
+
+        do{
+            System.out.println(num++);
+
+        }while(num<10);
+
+
+    }
+}
